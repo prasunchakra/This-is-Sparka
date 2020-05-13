ebcf70bd9b9aca3fb4484efe8da45b681dab2729
Prasun Chakraborty <lordprasun@gmail.com>
2020-05-13 09:07:21 +0530

    refactoring producer

--- Spark03_Kafka/_01_kafkaProducer.py
+++ Spark03_Kafka/_01_kafkaProducer.py
@@ -1,5 +1,3 @@
-import sys
-import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
@@ -9,20 +7,20 @@ import pykafka
 
 class TweetsListener(StreamListener):
 
-    def __init__(self, kafkaProducer):
-
+    def __init__(self, kafka_producer):
+        super().__init__()
         print("Tweets producer initialized")
-        self.producer = kafkaProducer
+        self.producer = kafka_producer
 
     def on_data(self, data):
 
         try:
             json_data = json.loads(data)
             words = json_data["text"].split()
-            hashtagList = list(filter(lambda x: x.lower().startswith("#"), words))
+            hash_tags = list(filter(lambda x: x.lower().startswith("#"), words))
 
-            if (len(hashtagList) != 0):
-                for hashtag in hashtagList:
+            if len(hash_tags) != 0:
+                for hashtag in hash_tags:
                     print(hashtag)
                     self.producer.produce(bytes(hashtag, "utf-8"))
 
@@ -32,23 +30,25 @@ class TweetsListener(StreamListener):
         return True
 
     def on_error(self, status):
-
         print(status)
         return True
 
 
-def connect_to_twitter(kafkaProducer, tracks):
+def connect_to_twitter(kafka_producer, tracks):
+    api_key = " "
+    api_secret = " "
 
+    access_token = " "
+    access_token_secret = " "
 
     auth = OAuthHandler(api_key, api_secret)
     auth.set_access_token(access_token, access_token_secret)
 
-    twitter_stream = Stream(auth, TweetsListener(kafkaProducer))
+    twitter_stream = Stream(auth, TweetsListener(kafka_producer))
     twitter_stream.filter(track=tracks, languages=["en"])
 
 
 if __name__ == "__main__":
-
     host = "127.0.0.1"
     port = "9092"
     topic = "tweet_analytics"
